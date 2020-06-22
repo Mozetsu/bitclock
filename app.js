a9772727c05ed4425ec38c6b79e523848ebb3cfd
Mozetsu <code.moz.00@gmail.com>
2020-06-22 01:11:04 +0100

    Refactor Clock Class

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ import { changeTheme, setSmoothTransition } from './js/interface.js';
 import { Clock } from './js/clock.js';
 
 const Bitclock = new Clock();
-Bitclock.initialize();
+// Bitclock.initialize();
+Bitclock.update(); // imediately update clock on page load
 
 const bodyHTML = document.querySelector('body');
 const themeBtn = document.querySelector('.themeBtn');
 themeBtn.addEventListener('click', () => changeTheme(bodyHTML));
 
-Bitclock.update(); // imediately updates time on page load
-setInterval(() => Bitclock.update(), 1000); // set interval of 1 sec to update time
-
 window.onload = () => setSmoothTransition(bodyHTML); // set smooth theme transition after page is loadaded
+
+setInterval(() => Bitclock.update(), 200); // update clock every second
--- js/clock.js
+++ js/clock.js
@@ -12,6 +12,8 @@ export class Clock {
 			Seconds: [],
 		};
 
+		this.timeUnit = {};
+
 		for (const unit in this.time) {
 			for (let i = 0; i < 2; i++) {
 				this.time[unit][i] = elem.querySelector(`.${unit}`).children[i]; // HTML time
@@ -20,26 +22,20 @@ export class Clock {
 		}
 	}
 
-	initialize() {
+	update() {
 		this.globalTime = new Date();
-		this.timeUnit = {};
 
 		for (const unit in this.time) {
-			const tmp = `get${unit}`;
-			// Get seconds --------------------------
-			this.timeUnit[unit] = this.globalTime[tmp]().toString().split('');
+			this.timeUnit[unit] = this.globalTime[`get${unit}`]().toString().split('');
 			if (this.timeUnit[unit].length === 1) this.timeUnit[unit].unshift(0);
 		}
-	}
 
-	update() {
-		this.initialize();
 		for (const unit in this.timeUnit) {
 			for (let n = 0; n < this.timeUnit[unit].length; n++) {
 				// Update HMTL hour ------------------------------------
 				this.time[unit][n].innerHTML = this.timeUnit[unit][n];
 				// Update HMTL binary cells ------------------------------------
-				const arr = this.decimalToBinary(parseInt(this.timeUnit[unit][n]));
+				const arr = this.decimalToBinary(this.timeUnit[unit][n]);
 				arr.forEach((bit, i) => {
 					if (bit > 0) return this.bits[unit][n].children[i].classList.add('active');
 					this.bits[unit][n].children[i].classList.remove('active');
