c9aeaf1e9c1e517505d4877585f137dba2d6b162
Mozetsu <code.moz.00@gmail.com>
2020-06-22 15:51:44 +0100

    Refactor app.js code

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ import { Clock } from './js/clock.js';
 
 const Bitclock = new Clock();
 Bitclock.update(); // imediately update clock on page load
+setInterval(() => Bitclock.update(), 200); // update clock every 200ms
 
 const bodyHTML = document.querySelector('body');
+bodyHTML.onload = () => setSmoothTransition(bodyHTML); // set smooth theme transition after body is loaded
+
 const themeBtn = document.querySelector('.themeBtn');
 themeBtn.addEventListener('click', () => changeTheme(bodyHTML));
-
-window.onload = () => setSmoothTransition(bodyHTML); // set smooth theme transition after page is loadaded
-
-setInterval(() => Bitclock.update(), 200); // update clock every second
